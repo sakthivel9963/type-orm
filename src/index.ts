327e316d1ef90739fdbc5a9c37db60ae7acb15d9
sakthivel9963 <sakthivel9963@gmail.com>
2021-10-13 02:10:42 +0530

    :tada: added type-orm, docker file and config for the database

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,33 @@
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import { User } from './entity/User';
 import app from './app';
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`app listening on url : http://localhost:${PORT}`);
-  console.log(`http://localhost:${PORT}/api/v1/ping`);
-});
+const main = async () => {
+  try {
+    const connection = await createConnection();
+
+    // console.log('Inserting a new user into the database...');
+    // const user = new User();
+    // user.firstName = 'Timber';
+    // user.lastName = 'Saw';
+    // user.age = 25;
+    // await connection.manager.save(user);
+    // console.log('Saved a new user with id: ' + user.id);
+
+    // console.log('Loading users from the database...');
+    // const users = await connection.manager.find(User);
+    // console.log('Loaded users: ', users);
+
+    // create express server
+    app.listen(PORT, () => {
+      console.log(`listening on port : http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+main();
